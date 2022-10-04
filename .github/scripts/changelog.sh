466d7f117743a858d3c440af7fc78ff66bf1d714
Aurelien Requiem <aurelien@requiem.fr>
2022-10-04 20:12:05 +1100

    remove debug

--- .github/scripts/changelog.sh
+++ .github/scripts/changelog.sh
@@ -70,7 +70,7 @@ if [ ! -z "$(echo $CURRENT_VERSION | sed  '/-\(alpha\|beta\|rc\)/!d')" ]; then
     GH_OPTS="--prerelease"
 fi
 
-echo gh release create "$CURRENT_VERSION" --notes-file "$RELEASE_CHANGELOG" $GH_OPTS
+gh release create "$CURRENT_VERSION" --notes-file "$RELEASE_CHANGELOG" $GH_OPTS
 
 sleep 2
 
